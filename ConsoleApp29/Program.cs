ea79127b713c4c533b1a8c9a7500d6b3d2181dc0
Burak Başaran <111579457+BurakBsrn@users.noreply.github.com>
2023-02-13 18:22:24 +0300

    Add files via upload

---
+++ ConsoleApp29/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            char[] karakter = new char[36]; 
+            int i = 0;
+
+            
+            for (char k = 'a'; k <= 'z'; k++) 
+            {
+                karakter[i] = k;
+                i++;
+            }
+
+            
+            for (char k = '0'; k <= '9'; k++)
+            {
+                karakter[i] = k;
+                i++;
+            }
+
+
+            Console.WriteLine();
+
+            
+            string sifre = ""; 
+            int dizisayaci = 0;
+
+            Random uret = new Random(); 
+            for (i = 0; i <= 9; i++)
+            {
+                dizisayaci = uret.Next(0, 35); 
+
+                
+                sifre = sifre + karakter[dizisayaci]; 
+            }
+
+            
+            Console.WriteLine("Üretilmiş olan şifre = " + sifre);
+
+            Console.ReadKey();
+
+        }
+    }
+}
